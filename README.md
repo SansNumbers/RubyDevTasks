f0d1a185df094fb0d62fae4a7f0723be46ded6ae
SansNumbers <kreig.antipin@gmail.com>
2021-10-27 15:50:26 +0300

    task 2 completed

--- README.md
+++ README.md
@@ -11,3 +11,25 @@
 
 - To run the ruby file, type in Terminal:
   `ruby script.rb`
+
+### Task 2 for Lecture #3
+
+- To run the ruby file, enter in Task 2 folder and type in Terminal:
+  `ruby script.rb`
+
+Console output should be following:
+`Drinks in stock:
+["Cola", "Water", "Juice"]
+
+Affordable drinks:
+{:price=>40.0, :name=>"Pepsi", :quantity_by_size=>{}}
+{:price=>39.99, :name=>"Water", :quantity_by_size=>{:l033=>1, :l2=>4}}
+
+Drinks out of stock:
+{:price=>40.0, :name=>"Pepsi", :quantity_by_size=>{}}
+
+How much Cola drinks left in stock:
+{:l033=>3, :l05=>7, :l1=>8, :l2=>4}
+
+Total amount of drinks:
+36`
---
+++ Task 2/script.rb
@@ -0,0 +1,62 @@
+class Inventory
+
+    def initialize(array)
+        @arr = array
+    end
+
+    def item_in_stock
+        array = []
+        @arr.each { |drink| array << drink[:name] if drink[:quantity_by_size].length > 0}
+        print array
+    end
+
+    def affordable
+        array = []
+        @arr.each { |drink| array << drink if drink[:price] < 50}
+        puts array
+    end
+
+    def out_of_stock
+        array = []
+        @arr.each { |drink| array << drink if drink[:quantity_by_size].length == 0}
+        puts array
+    end
+
+    def how_much_left(name)
+        array = []
+        @arr.each { |drink| return drink[:quantity_by_size] if drink[:name] == name}
+        puts array
+    end
+
+    def total_stock
+        sum = 0
+        @arr.each { |drink| drink[:quantity_by_size].each { |litrage, quantity| sum = sum + quantity }}
+        print sum
+    end
+end
+
+inventory = [
+    {price: 125.00, name: "Cola", quantity_by_size: {l033: 3, l05: 7, l1: 8, l2: 4}},
+    {price: 40.00, name: "Pepsi", quantity_by_size: {}},
+    {price: 39.99, name: "Water", quantity_by_size: {l033: 1, l2: 4}},
+    {price: 70.00, name: "Juice", quantity_by_size: {l033: 7, l05: 2}}
+]
+
+drink = Inventory.new(inventory)
+
+puts 'Drinks in stock: '
+puts drink.item_in_stock
+puts
+
+puts 'Affordable drinks: '
+puts drink.affordable
+
+puts 'Drinks out of stock: '
+puts drink.out_of_stock
+
+puts 'How much Cola drinks left in stock: '
+puts drink.how_much_left("Cola")
+puts
+
+puts 'Total amount of drinks: '
+print drink.total_stock
